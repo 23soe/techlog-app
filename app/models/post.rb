69c687f68b7854fd9d11f7a900287f03e53b7358
JEONG SOEE <jj.soxee@gmail.com>
2025-01-28 22:22:52 +0900

    fix : PostgreSQL migration バグ修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
     belongs_to :user
     belongs_to :category
 
-    has_and_belongs_to_many :tags, join_table: :post_tags
+    has_and_belongs_to_many :tags, join_table: :posts_tags
   
     validates :title, presence: true, length: { maximum: 100 }
     validates :content, presence: true, length: { maximum: 100 }
@@ -25,32 +25,22 @@ class Post < ApplicationRecord
     # タグの解析と保存
     def parse_and_save_tags
         # content から全角・半角のハッシュタグを抽出
-        # puts "Processing content: #{content.inspect}" # デバッグ用
         matches = content.scan(/[＃#][\p{Word}\p{Alnum}_]+/u)
-        # puts "Extracted matches: #{matches.inspect}"
         return if matches.empty?
         
         matches.flatten.each do |tag_name|
-            # puts "Processing tag # #{tag_name.inspect}" # デバッグ用
             normalized_tag_name = tag_name.unicode_normalize(:nfkc).gsub('＃', '#')
-            # puts "Normalized tag name: #{normalized_tag_name.inspect}" # デバッグ用
         
             begin
             tag = Tag.find_or_create_by(name: normalized_tag_name)
-            # puts "Found or created tag: #{tag.inspect}" # デバッグ用
         
             if tags.include?(tag)
-                # puts "Tag already associated with this post: #{tag.name}" # デバッグ用
             else
                 tags << tag
-                # puts "Tag associated with this post: #{tag.name}" # デバッグ用
             end
             rescue => e
-            # puts "Error processing tag: #{e.message}" # デバッグ用
             end
         end
-        
-        # puts "Final tags for this post: #{self.tags.map(&:name)}" # デバッグ用
     end
 
     def normalize_content
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-    has_and_belongs_to_many :posts, join_table: :post_tags
+    has_and_belongs_to_many :posts, join_table: :posts_tags
 
     validates :name, presence: true, uniqueness: true
 end
